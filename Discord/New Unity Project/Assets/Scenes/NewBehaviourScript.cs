575c74e4f70795d8e687c3033717c91613b3f9d1
BEN OTHMAN SAMY p1707136 <samy.ben-othman@etu.univ-lyon1.fr>
2020-05-19 02:20:55 +0200

    test

---
+++ Discord/New Unity Project/Assets/Scenes/NewBehaviourScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public DragMouseMove dm;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Wall"))
+        {
+           
+        }
+    }
+}
